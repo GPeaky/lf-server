2ab16693c292588c4aea9677f9236295d776d56c
GPeaky <gerardjoven2020@hotmail.com>
2021-12-21 16:07:56 +0100

    feat(SuperUser): Updated tpm command, added tpm with vehicle and message when you has been teleported

--- client_packages/SuperUser/index.js
+++ client_packages/SuperUser/index.js
@@ -3,15 +3,15 @@ mp.events.add('playerCommand', command => {
     if(command === 'tpm') {
         if (mp.game.invoke('0x1DD1F58F493F1DA5')) {
             let blipIterator = mp.game.invoke('0x186E5D252FA50E7D');
-            let FirstInfoId = mp.game.invoke('0x1BEDE233E6CD2A1F', blipIterator);
-            let NextInfoId = mp.game.invoke('0x14F96AA50D6FBEA7', blipIterator);
+            const FirstInfoId = mp.game.invoke('0x1BEDE233E6CD2A1F', blipIterator);
+            const NextInfoId = mp.game.invoke('0x14F96AA50D6FBEA7', blipIterator);
             for (let i = FirstInfoId; mp.game.invoke('0xA6DB27D19ECBB7DA', i) != 0; i = NextInfoId) {
                 if (mp.game.invoke('0xBE9B0959FFD0779B', i) == 4) {
-                    let oldpos = mp.players.local.position;
+                    const oldpos = mp.players.local.position;
                     let coord = mp.game.ui.getBlipInfoIdCoord(i);
     
                     coord.z = mp.game.gameplay.getGroundZFor3dCoord(coord.x, coord.y, i * 50, 0, false); // try calcualte Z
-                    mp.players.local.position = coord;
+                    mp.players.local.setCoordsKeepVehicle(coord.x, coord.y, coord.z);
                       
                     mp.players.local.freezePosition(true);
                     setTimeout(() => { // let the game load the map
@@ -22,13 +22,14 @@ mp.events.add('playerCommand', command => {
                         }
     
                         if (coord.z != 0) { // if found groundZ
-                            mp.players.local.position = coord;
+                            mp.players.local.setCoordsKeepVehicle(coord.x, coord.y, coord.z);
+                            mp.gui.chat.push(`You have been teleported`);
                         } else {
-                            mp.players.local.position = oldpos;
+                            mp.players.local.setCoordsKeepVehicle(oldpos.x, oldpos.y, oldpos.z);
                             mp.gui.chat.push("Could not find elevation at waypoint position!");
                         }
                         mp.players.local.freezePosition(false);
-                    }, 1500);
+                    }, 2000);
                 }
             }
         }
