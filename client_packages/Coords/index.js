a06b8dfb2ed960d55b16eb6c81d474436e149c7f
GPeaky <gerardjoven2020@hotmail.com>
2021-12-21 22:38:30 +0100

    feat(coords): Modified css

--- client_packages/Coords/index.js
+++ client_packages/Coords/index.js
@@ -1,14 +1,14 @@
 const browser = mp.browsers.new('package://Coords/index.html');
-browser.active= false;
+const player = mp.players.local;
+browser.active= true;
 
 mp.events.add('viewCoords', () => {
-    const player = mp.players.local;
     browser.active = !browser.active;
+})
 
-    mp.events.add('render', () => {
+mp.events.add('render', () => {
 
-        if (browser.active) {
-            browser.execute(`setCoords(${player.position.x.toFixed(2)}, ${player.position.y.toFixed(2)}, ${player.position.z.toFixed(2)})`);
-        }
-    })
+    if (browser.active) {
+        browser.execute(`setCoords(${player.position.x.toFixed(2)}, ${player.position.y.toFixed(2)}, ${player.position.z.toFixed(2)})`);
+    }
 })
