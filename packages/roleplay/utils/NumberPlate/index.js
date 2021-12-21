ed181a2aec8bb162f3bfbc92574766b888558bb6
GPeaky <gerardjoven2020@hotmail.com>
2021-12-21 21:11:10 +0100

    feat(utils): Added one more character to mp.utils.generateNumberPlate

--- packages/roleplay/utils/NumberPlate/index.js
+++ packages/roleplay/utils/NumberPlate/index.js
@@ -2,7 +2,7 @@ mp.utils.generateNumberPlate = () => {
     let numberPlate = '';
     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
 
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i <= 7; i++) {
         numberPlate += letters[Math.floor(Math.random() * letters.length)];
     }
 
