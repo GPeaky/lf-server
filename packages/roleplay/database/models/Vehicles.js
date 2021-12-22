a29bff005a69df6d5661bbd4b05a722371adb433
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2021-12-22 03:21:44 +0100

    feat(Vehicles): Remove ID from Model

--- packages/roleplay/database/models/Vehicles.js
+++ packages/roleplay/database/models/Vehicles.js
@@ -2,12 +2,6 @@ const Sequelize = require('sequelize');
 const sequelize = require('../../config/database');
 
 const Vehicles = sequelize.define('vehicles', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true
-    },
-
     model: {
         type: Sequelize.STRING,
         allowNull: false
