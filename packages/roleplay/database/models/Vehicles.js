38dcec88b7f408a91b8203a342022abcc61c0319
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2021-12-22 17:29:09 +0100

    fix(Vehicles): Fixed error with id

--- packages/roleplay/database/models/Vehicles.js
+++ packages/roleplay/database/models/Vehicles.js
@@ -2,6 +2,12 @@ const Sequelize = require('sequelize');
 const sequelize = require('../../config/database');
 
 const Vehicles = sequelize.define('vehicles', {
+    id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+
     model: {
         type: Sequelize.STRING,
         allowNull: false
