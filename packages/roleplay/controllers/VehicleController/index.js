38caa179cf8ef804485f8178515a67fb6bf631cf
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2021-12-22 15:40:02 +0100

    feat(VehicleController): Added boot VehicleController

--- packages/roleplay/controllers/VehicleController/index.js
+++ packages/roleplay/controllers/VehicleController/index.js
@@ -1,25 +1,57 @@
 const Vehicles = require('../../database/models/Vehicles');
 
-const Instantiate = async vehicle => {
-    Vehicles.create({
-        model: vehicle.model,
-        data: 'test'
+const Instantiate = vehicle => {
+    vehicleData = JSON.stringify({
+        position: vehicle.position,  
+        color: vehicle.getColorRGB(),
+        dimension: vehicle.dimension,
+        heading: vehicle.heading
     })
+    Vehicles.create({
+        id: vehicle.numberPlate,
+        model: vehicle.model,
+        data: vehicleData,
+    })
+    vehicle.isPersistent = true
 }
 
-const main = async() => {
-    const result = await Vehicles.findAll({
+const Save = vehicle => {
+    vehicleData = JSON.stringify({
+        position: vehicle.position,  
+        color: vehicle.getColorRGB(),
+        dimension: vehicle.dimension,
+        heading: vehicle.heading
+    })
+    Vehicles.update({ data: vehicleData }, {
         where: {
-            id: 1
+            id: vehicle.numberPlate
         }
     })
-    for await (const item of result) {
-
-    }
-} 
-
-main()
-
-module.exports = {
-    Instantiate
 }
+
+const spawnVehicle = ({ id, model, data }) => {
+    const vehicleData = JSON.parse(data)
+    const vehicle = mp.vehicles.new(mp.joaat(model), vehicleData.position, {
+        engine: false,
+        heading: vehicleData.heading,
+        dimension: vehicleData.dimension,
+        numberPlate: id,
+    })
+    vehicle.setColorRGB(vehicleData.color)
+    vehicle.isPersistent = true
+    console.log(`Vehicle with ID: ${id} spawned.`)
+}
+
+const bootVehicles = async() => {
+    const result = await Vehicles.findAll({})
+    for await (const vehicle of result) {
+        spawnVehicle(vehicle)
+    }  
+}
+
+bootVehicles()
+
+module.exports = {Instantiate, Save}
+
+// VehicleSaver
+require('./VehicleSaver')
