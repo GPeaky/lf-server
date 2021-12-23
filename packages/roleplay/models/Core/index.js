f4c19aa4b1f06415f2f7bd34e27ef910db505d9a
NachoASD <nacheteasd@gmail.com>
2021-12-23 20:09:57 +0100

    Feat(Label): Added mp.core.Label class to create floating text

--- packages/roleplay/models/Core/index.js
+++ packages/roleplay/models/Core/index.js
@@ -1,3 +1,58 @@
 mp.core = {}
 
 mp.core.wait = async(ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+mp.core.Label = class {
+    constructor(text, position, options={}, keyOptions=null) {
+        this.text = text;
+        this.position = position;
+        this.options = options;
+        this.inColshape = false;
+        if(keyOptions){
+            const { key, coords, callback } = keyOptions;
+            this.key = key;
+            this.coords = coords;
+            this.callback = callback;
+            this.colshape = mp.colshapes.newRectangle(this.coords.x, this.coords.y, this.coords.width, this.coords.height, this.coords.dimension)
+            console.log(this.colshape)
+            this.initKey();
+        }
+    }
+
+    initKey() {
+        
+        const playerEnterColshapeHandler = (player, shape) => {
+            console.log("Player enter colshape", shape, this.colshape, shape == this.colshape);
+            if(shape == this.colshape) {
+                this.inColshape = true;
+                console.log("Closhape", this.colshape);
+            }
+        }
+          
+        mp.events.add("playerEnterColshape", playerEnterColshapeHandler);
+
+        const playerExitColshapeHandler = (player, shape) => {
+            console.log("Player exit colshape", shape);
+            if(shape == this.colshape) {
+                this.inColshape = false;
+                console.log("Closhape", this.colshape);
+            }
+        }
+
+        mp.events.add("playerExitColshape", playerExitColshapeHandler);
+
+        console.log(`Creating event to Keydown: ${this.key.toString(16)}`);
+
+        mp.events.add(`Keyup::${this.key.toString(16)}`, (  ) => {
+            console.log(this.key.toString(16))
+            console.log(this.inColshape)
+            if(this.inColshape) {
+                this.callback()
+            }
+        });
+    }
+
+    draw() {
+        mp.labels.new(this.text, this.position, this.options);
+    }
+}
--- packages/roleplay/tests/index.js
+++ packages/roleplay/tests/index.js
@@ -1,1 +1,18 @@
 //* You can run this file to test your code.
+
+const Label = new mp.core.Label("texttexttexttexttexttext", new mp.Vector3(-431.88, 1146.86, 327), {
+    los: false,
+    font: 1,
+    drawDistance: 100,
+}, {
+    key: 0x42,
+    coords: {x: -431.88, y: 1146.86, z: 327, width: 5, height: 5, dimension: 0},
+    callback: () => {
+        console.log("Pulsooow");
+    }
+})
+
+mp.events.addCommand('TestLabel', player => {
+    Label.draw()
+    player.position =  new mp.Vector3(-431.88, 1146.86, 327)
+});
