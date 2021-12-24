955f77106edb6bd3932e6eb811929dfdd677a6c2
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2021-12-24 23:08:29 +0100

    feat(Player): Removed playerLeft event

--- packages/roleplay/scripts/Player/events.js
+++ packages/roleplay/scripts/Player/events.js
@@ -3,14 +3,3 @@ const { SERVER_NAME } = process.env
 
 mp.events.add('playerJoin', player => console.log(`[${SERVER_NAME.yellow}] - ${player.name} joined to the server` ))
 mp.events.add('playerLeft', player => console.log(`[${SERVER_NAME.yellow}] - ${player.name} left the server` ))
-
-
-mp.events.add('playerReady', async (player) => {
-    const exits = await player.exist()
-    console.log(exits)
-    if(!exits){
-        player.create()
-    }else{
-        LoadPlayer(player)
-    }
-})
