83c4223029f378133ca8141ea8d8a0f02cb82428
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2021-12-24 05:57:36 +0100

    feat(Player): Migrated some functions to model functions

--- packages/roleplay/models/Player/index.js
+++ packages/roleplay/models/Player/index.js
@@ -1,9 +1,79 @@
 const { Instantiate, Remove } = require('../../scripts/Vehicle/controller')
 
-mp.events.add('playerReady', (player) => {
+mp.events.add('playerReady', player => {
     player.name = `${player.ip} - ${player.id} - ${player.socialClub}`
 
     // Player <=> Vehicles
+
+    player.create = async() => {
+        await mp.utils.wait(750)
+
+    player.position = new mp.Vector3(-61.71 , -1218.14 , 28.7);
+
+    player.heading = 248.88;
+    player.dimension = 0;
+    player.health = 100;
+
+    // Set Default Data
+
+    player.setClothes(0, 0, 0, 0);
+    player.setClothes(1, 0, 0, 0);
+    player.setClothes(2, 12, 0, 0);
+    player.setClothes(3, 8, 0, 0);
+    player.setClothes(4, 4, 0, 0);
+    player.setClothes(5, 0, 0, 0);
+    player.setClothes(6, 10, 0, 0);
+    player.setClothes(7, 0, 0, 0);
+    player.setClothes(8, 4, 0, 0);
+    player.setClothes(9, 0, 0, 0);
+    player.setClothes(10, 0, 0, 0);
+    player.setClothes(11, 4, 0, 0);
+
+    await mp.utils.wait(150)
+
+    const playerData = JSON.stringify({
+        position: player.position,
+        dimension: player.dimension,
+        heading: player.heading,
+        health: 100,
+        armor: 0,
+
+        clothes: [
+            [   player.getClothes(0)   ],
+            [   player.getClothes(1)   ],
+            [   player.getClothes(2)   ],
+            [   player.getClothes(3)   ],
+            [   player.getClothes(4)   ],
+            [   player.getClothes(5)   ],
+            [   player.getClothes(6)   ],
+            [   player.getClothes(7)   ],
+            [   player.getClothes(8)   ],
+            [   player.getClothes(9)   ],
+            [   player.getClothes(10)  ],
+            [   player.getClothes(11)  ]
+        ],
+        haircolor: [player.getHairColor, player.getHairHighlightColor],
+    })
+
+    mp.database.Players.create({
+        id: player.serial,
+        name: player.name,
+        email: "player.email",
+        password: "player.password",
+        data: playerData
+    })
+    }
+
+    player.exist = async () => {
+        const PlayerDB = await mp.database.Players.findOne({
+            where: {
+                id: player.serial
+            }
+        })
+        
+        if (PlayerDB) return true
+        return false
+    }
 
     player.spawnVehicle = (vehicle, position, heading) => {
         if (vehicle) {
--- packages/roleplay/scripts/Player/controller.js
+++ packages/roleplay/scripts/Player/controller.js
@@ -1,66 +1,10 @@
-const Player = require('../../database/models/Players');
-
 const CreatePlayer = async (player) => {
 
-    await mp.core.wait(750)
-
-    player.position = new mp.Vector3(-61.71 , -1218.14 , 28.7);
-
-    player.heading = 248.88;
-    player.dimension = 0;
-    player.health = 100;
-
-    // Set Default Data
-
-    player.setClothes(0, 0, 0, 0);
-    player.setClothes(1, 0, 0, 0);
-    player.setClothes(2, 12, 0, 0);
-    player.setClothes(3, 8, 0, 0);
-    player.setClothes(4, 4, 0, 0);
-    player.setClothes(5, 0, 0, 0);
-    player.setClothes(6, 10, 0, 0);
-    player.setClothes(7, 0, 0, 0);
-    player.setClothes(8, 4, 0, 0);
-    player.setClothes(9, 0, 0, 0);
-    player.setClothes(10, 0, 0, 0);
-    player.setClothes(11, 4, 0, 0);
-
-    await mp.core.wait(150)
-
-    const playerData = JSON.stringify({
-        position: player.position,
-        dimension: player.dimension,
-        heading: player.heading,
-        health: 100,
-        armor: 0,
-
-        clothes: [
-            [   player.getClothes(0)   ],
-            [   player.getClothes(1)   ],
-            [   player.getClothes(2)   ],
-            [   player.getClothes(3)   ],
-            [   player.getClothes(4)   ],
-            [   player.getClothes(5)   ],
-            [   player.getClothes(6)   ],
-            [   player.getClothes(7)   ],
-            [   player.getClothes(8)   ],
-            [   player.getClothes(9)   ],
-            [   player.getClothes(10)  ],
-            [   player.getClothes(11)  ]
-        ],
-        haircolor: [player.getHairColor, player.getHairHighlightColor],
-    })
-
-    Player.create({
-        id: player.serial,
-        name: player.name,
-        email: "player.email",
-        password: "player.password",
-        data: playerData
-    })
 }
 
 const SavePlayer = (player) => {
+    if (!player.loaded) return
+
     const playerData = JSON.stringify({
         position: player.position,
         dimension: player.dimension,
@@ -84,26 +28,15 @@ const SavePlayer = (player) => {
         ],
         haircolor: [player.getHairColor, player.getHairHighlightColor],
     })
-    Player.update({ data: playerData }, {
+    mp.database.Players.update({ data: playerData }, {
         where: {
             id: player.serial
         }
     })
-}
-
-const ExistPlayer = async(player) => {
-    const PlayerDB = await Player.findOne({
-        where: {
-            id: player.serial
-        }
-    })
-    
-    if (PlayerDB) return true
-    return false
 }
 
 const LoadPlayer = async (player) => {
-    const { data } = await Player.findOne({
+    const { data } = await mp.database.Players.findOne({
         where: {
             id: player.serial
         }
@@ -111,7 +44,7 @@ const LoadPlayer = async (player) => {
 
     if(player && data) {
 
-        await mp.core.wait(750)
+        await mp.utils.wait(750)
 
         const playerData = JSON.parse(data)
                 
@@ -120,9 +53,9 @@ const LoadPlayer = async (player) => {
         player.heading = playerData.heading
         player.health = playerData.health
         player.armour = playerData.armor
+        player.loaded = true
 
         playerData.clothes.forEach((clothes, index) => {
-            // console.log(index, clothes)
             player.setClothes(parseInt(index), parseInt(clothes[0].drawable) , parseInt(clothes[0].texture), 0)
         })
 
@@ -130,6 +63,6 @@ const LoadPlayer = async (player) => {
     }
 }
 
-module.exports = { CreatePlayer, SavePlayer, ExistPlayer, LoadPlayer }
+module.exports = { CreatePlayer, SavePlayer, LoadPlayer }
 
 require('./PlayerSaver')
