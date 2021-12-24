25a1dc5a126005d6f01edbecf575a794d594d388
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2021-12-24 23:08:50 +0100

    feat(Player): SavePlayer migrated

--- packages/roleplay/models/Player/index.js
+++ packages/roleplay/models/Player/index.js
@@ -5,73 +5,157 @@ mp.events.add('playerReady', player => {
 
     // Player <=> Vehicles
 
-    player.create = async() => {
+    player.create = async(email, password) => {
+        if (player.loaded) return
         await mp.utils.wait(750)
+        
+        player.position = new mp.Vector3(-61.71 , -1218.14 , 28.7);
+        
+        player.heading = 248.88;
+        player.dimension = 0;
+        player.health = 100;
+        
+        // Set Default Data
+        
+        player.setClothes(0, 0, 0, 0);
+        player.setClothes(1, 0, 0, 0);
+        player.setClothes(2, 12, 0, 0);
+        player.setClothes(3, 8, 0, 0);
+        player.setClothes(4, 4, 0, 0);
+        player.setClothes(5, 0, 0, 0);
+        player.setClothes(6, 10, 0, 0);
+        player.setClothes(7, 0, 0, 0);
+        player.setClothes(8, 4, 0, 0);
+        player.setClothes(9, 0, 0, 0);
+        player.setClothes(10, 0, 0, 0);
+        player.setClothes(11, 4, 0, 0);
+        
+        await mp.utils.wait(150)
+        
+        const playerData = JSON.stringify({
+            position: player.position,
+            dimension: player.dimension,
+            heading: player.heading,
+            health: 100,
+            armor: 0,
 
-    player.position = new mp.Vector3(-61.71 , -1218.14 , 28.7);
-
-    player.heading = 248.88;
-    player.dimension = 0;
-    player.health = 100;
-
-    // Set Default Data
-
-    player.setClothes(0, 0, 0, 0);
-    player.setClothes(1, 0, 0, 0);
-    player.setClothes(2, 12, 0, 0);
-    player.setClothes(3, 8, 0, 0);
-    player.setClothes(4, 4, 0, 0);
-    player.setClothes(5, 0, 0, 0);
-    player.setClothes(6, 10, 0, 0);
-    player.setClothes(7, 0, 0, 0);
-    player.setClothes(8, 4, 0, 0);
-    player.setClothes(9, 0, 0, 0);
-    player.setClothes(10, 0, 0, 0);
-    player.setClothes(11, 4, 0, 0);
-
-    await mp.utils.wait(150)
-
-    const playerData = JSON.stringify({
-        position: player.position,
-        dimension: player.dimension,
-        heading: player.heading,
-        health: 100,
-        armor: 0,
-
-        clothes: [
-            [   player.getClothes(0)   ],
-            [   player.getClothes(1)   ],
-            [   player.getClothes(2)   ],
-            [   player.getClothes(3)   ],
-            [   player.getClothes(4)   ],
-            [   player.getClothes(5)   ],
-            [   player.getClothes(6)   ],
-            [   player.getClothes(7)   ],
-            [   player.getClothes(8)   ],
-            [   player.getClothes(9)   ],
-            [   player.getClothes(10)  ],
-            [   player.getClothes(11)  ]
-        ],
-        haircolor: [player.getHairColor, player.getHairHighlightColor],
-    })
-
-    mp.database.Players.create({
-        id: player.serial,
-        name: player.name,
-        email: "player.email",
-        password: "player.password",
-        data: playerData
-    })
+            clothes: [
+                [   player.getClothes(0)   ],
+                [   player.getClothes(1)   ],
+                [   player.getClothes(2)   ],
+                [   player.getClothes(3)   ],
+                [   player.getClothes(4)   ],
+                [   player.getClothes(5)   ],
+                [   player.getClothes(6)   ],
+                [   player.getClothes(7)   ],
+                [   player.getClothes(8)   ],
+                [   player.getClothes(9)   ],
+                [   player.getClothes(10)  ],
+                [   player.getClothes(11)  ]
+            ],
+            haircolor: [player.getHairColor, player.getHairHighlightColor],
+        })
+        
+        mp.database.Players.create({
+            username: player.name,
+            email: email,
+            password: password,
+            data: playerData
+        })
     }
 
-    player.exist = async () => {
-        const PlayerDB = await mp.database.Players.findOne({
+    player.save = async () => {
+        if (!player.loaded) return
+        
+        const playerData = {
+            position: player.position,
+            dimension: player.dimension,
+            heading: player.heading,
+            health: player.health,
+            armor: player.armour,
+            lastVehicle: player.vehicle ? {numberPlate: player.vehicle.numberPlate, seat: player.seat} : false,
+            clothes: [
+                [   player.getClothes(0)   ],
+                [   player.getClothes(1)   ],
+                [   player.getClothes(2)   ],
+                [   player.getClothes(3)   ],
+                [   player.getClothes(4)   ],
+                [   player.getClothes(5)   ],
+                [   player.getClothes(6)   ],
+                [   player.getClothes(7)   ],
+                [   player.getClothes(8)   ],
+                [   player.getClothes(9)   ],
+                [   player.getClothes(10)  ],
+                [   player.getClothes(11)  ]
+            ],
+            haircolor: [player.getHairColor, player.getHairHighlightColor],
+        }
+        
+        mp.database.Players.update({ data: JSON.stringify(playerData) }, {
             where: {
-                id: player.serial
+                username: player.username
+            }
+        })
+    }
+    
+    player.load = async (email) => {
+        if (player.loaded) return
+        const { data, username } = await mp.database.Players.findOne({
+            where: {
+                email: email
+            }
+        })
+
+        mp.players.forEach(_player => {
+            if (_player.id !== player.id) {
+                if (_player.username === username) _player.logout()
             }
         })
         
-        if (PlayerDB) return true
+        if(player && data) {
+            
+            const playerData = JSON.parse(data)
+
+            
+            player.position = playerData.position
+            player.dimension = playerData.dimension
+            player.heading = playerData.heading
+            player.health = playerData.health
+            player.armour = playerData.armor
+            player.username = username
+            player.name = username
+        
+            if (playerData.lastVehicle) {
+                mp.vehicles.forEach(vehicle => {
+                    console.log('founded')
+                    if (vehicle.numberPlate === playerData.lastVehicle?.numberPlate) player.putIntoVehicle(vehicle, playerData.lastVehicle?.seat)
+                })
+            }
+            
+            
+            playerData.clothes.forEach((clothes, index) => {
+                player.setClothes(parseInt(index), parseInt(clothes[0].drawable) , parseInt(clothes[0].texture), 0)
+            })
+        }
+        player.loaded = true
+        player.call('login:disable')
+    }
+
+    player.logout = async() => {
+        player.save()
+        player.loaded = false
+        player.dimension = player.id + 5000
+        player.call('login:enable')
+    }
+
+    player.exist = async (email, password) => {
+        const PlayerDB = await mp.database.Players.findOne({
+            where: {
+                email: email
+            }
+        })
+        
+        if (PlayerDB) return PlayerDB
         return false
     }
 
