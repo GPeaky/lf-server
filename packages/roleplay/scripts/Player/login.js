34fc2bd9dd02062c16708d743f25898157cc52f6
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2021-12-24 23:07:38 +0100

    feat(Player): Login server handler

---
+++ packages/roleplay/scripts/Player/login.js
@@ -0,0 +1,12 @@
+mp.events.add('playerReady', async player => player.logout())
+
+
+const handleLogin = async (player, email, password)  => {
+    const PlayerDB = await player.exist(email, password)
+    if (!PlayerDB) return player.create(email, password)
+    if (PlayerDB.password === password) {
+        player.load(email)
+    }
+}
+
+mp.events.add('submitLogin', handleLogin)
