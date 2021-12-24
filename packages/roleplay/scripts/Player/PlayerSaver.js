0acc74d822d7d21b4444066fb8ec9593bdfc7052
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2021-12-24 23:06:54 +0100

    feat(Player): Updated save player to player.save method

--- packages/roleplay/scripts/Player/PlayerSaver.js
+++ packages/roleplay/scripts/Player/PlayerSaver.js
@@ -1,10 +1,8 @@
-const { SavePlayer } = require('./controller.js')
-
 setInterval(() => {
     if(mp.players.length > 0) {
         mp.players.forEach(player => {
-            SavePlayer(player)
-        })
+            player.save()
+         })
         console.log(`Saved #${mp.players.length} Players`)
     }
 }, 5000);
