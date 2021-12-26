7d11ccc4fbd2a06cce532e5119ad73e0cc5ec498
Jose Poveda <75023397+Kilichi@users.noreply.github.com>
2021-12-26 19:36:10 +0100

    feat(speedometer) added only view conductor

--- client_packages/Browser/speedometer.js
+++ client_packages/Browser/speedometer.js
@@ -1,12 +1,23 @@
-const speedometerBrowser = mp.browsers.new('package://Cef/Speedometer/index.html');
-speedometerBrowser.active= false;
+const speedometerBrowser = mp.browsers.new('package://Cef/Speedometer/index.html')
+speedometerBrowser.active= false
 
-mp.events.add('render', () => {
-    if( player.vehicle ) {
-        speedometerBrowser.active = true;
-        const velocity = player.vehicle.getSpeed() * 3.6;
-        const gas = player.vehicle.getPetrolTankHealth();
-        const rpm = player.vehicle.rpm;
-        speedometerBrowser.execute(`setSpeedometer(${velocity.toFixed(0)}, ${rpm * 10000}, ${gas.toFixed(0)})`);
-    } else speedometerBrowser.active = false;
-}) 
+function playerEnterVehicleHandler(vehicle, seat) {
+    const player = mp.players.local
+    if (seat == -1) {
+        speedometerBrowser.active = true
+        setInterval(() => {
+            if (!player.vehicle) return
+            const velocity = player.vehicle.getSpeed() * 3.6
+            const gas = player.vehicle.getPetrolTankHealth()
+            const rpm = player.vehicle.rpm
+            speedometerBrowser.execute(`setSpeedometer(${velocity.toFixed(0)}, ${rpm * 10000}, ${gas.toFixed(0)})`)
+        }, 100)
+    }
+}
+
+function playerLeavedVehicleHandler(vehicle, seat){
+    speedometerBrowser.active= false
+}
+
+mp.events.add("playerEnterVehicle", playerEnterVehicleHandler)
+mp.events.add("playerLeaveVehicle", playerLeavedVehicleHandler)
