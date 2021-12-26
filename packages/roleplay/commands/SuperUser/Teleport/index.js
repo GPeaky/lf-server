aa29b7358ba07d4c413da2cb72598c1b651afa17
GPeaky <gerardjoven2020@hotmail.com>
2021-12-26 22:39:34 +0100

    feat(ipl): ADDED

--- packages/roleplay/commands/SuperUser/Teleport/index.js
+++ packages/roleplay/commands/SuperUser/Teleport/index.js
@@ -1,5 +1,11 @@
+// * Go To
 mp.events.addCommandGroup('goto', ['superUser'], (player, id) => {
     const targetPlayer = mp.players.at(id)
     if (!targetPlayer) return;
     player.position = targetPlayer.position
+})
+
+//* Teleport to coords
+mp.events.addCommandGroup('tp', ['superUser'], (player, position, x, y, z) => {
+    player.position = new mp.Vector3(Number(x), Number(y), Number(z));
 })
---
+++ packages/roleplay/ipl/index.js
@@ -0,0 +1,2 @@
+mp.world.requestIpl('vw_casino_carpark');
+mp.world.requestIpl('vw_casino_garage');
