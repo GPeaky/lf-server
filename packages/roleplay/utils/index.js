0bf95ee40454c69394e68b36db9be08c56d14870
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2021-12-31 18:36:28 +0100

    feat(Utils): Added mp.utils.Queue

--- packages/roleplay/utils/index.js
+++ packages/roleplay/utils/index.js
@@ -3,5 +3,35 @@ mp.utils = {}
 // Wait
 mp.utils.wait = async(ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+// Queue
+mp.utils.Queue = class {
+    constructor(maxSimultaneously = 1) {
+        this.maxSimultaneously = maxSimultaneously;
+        this.__active = 0;
+        this.__queue = [];
+    }
+
+    /** @param { () => Promise<T> } func 
+     * @template T
+     * @returns {Promise<T>}
+    */
+    async enqueue(func) {
+        if(++this.__active > this.maxSimultaneously) {
+            await new Promise(resolve => this.__queue.push(resolve));
+        }
+
+        try {
+            return await func();
+        } catch(err) {
+            throw err;
+        } finally {
+            this.__active--;
+            if(this.__queue.length) {
+                this.__queue.shift()();
+            }
+        }
+    }
+}
+
 // NumberPlate 
 require('./NumberPlate');
