1e1ad9e35a1a5d71a2a9c9c16778a8ba2d2fe6ba
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2022-01-05 03:33:08 +0100

    Reverted(Prisma): Removed shit

--- packages/roleplay/config/database.js
+++ packages/roleplay/config/database.js
@@ -3,23 +3,8 @@ const Sequelize = require('sequelize');
 
 const { SQ_NAME, SQ_USER, SQ_PASS } = process.env;
 
-<<<<<<< HEAD
-            getPlayerByWallet: async wallet => {
-                return await prisma.players.findUnique({
-                    data: {
-                        wallet
-                    }
-                })
-            }
-        },
-        Vehicles : prisma.vehicles,
-        Transactions : prisma.transactions,
-    }
-}
-=======
 module.exports = new Sequelize(SQ_NAME, SQ_USER, SQ_PASS, {
     host: 'localhost',
     dialect: 'mariadb',
     logging: log => logger.info(log)
 })
->>>>>>> parent of 2c57541 (feat(database): added prisma again)
