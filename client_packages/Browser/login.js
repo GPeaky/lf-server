b7fb3471e2b614afe72e0415331f25b6ebfa392a
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2022-01-07 19:29:02 +0100

    Feat(CameraLogin): More beautifull

--- client_packages/Browser/login.js
+++ client_packages/Browser/login.js
@@ -3,7 +3,7 @@ const loginBrowser = mp.browsers.new('package://Cef/Login/index.html');
 const Camera = require('./Browser/camera');
 let cam = new Camera('loginCam', new mp.Vector3(24.48141, -255.5137, 141.269), new mp.Vector3(-51.10479, -680.2582, 140.7251));
 
-cam.startMoving(50.0)
+cam.startMoving(5000.0)
 
 mp.events.add({
     'getInfo': (email, passwd) => {
