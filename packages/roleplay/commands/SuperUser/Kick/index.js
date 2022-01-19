1ed34735fac5b18e4da18e3a920698898be50eaf
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2022-01-19 16:43:53 +0100

    feat(Core): Added core to client-side and fixed Death blackscreen

--- packages/roleplay/commands/SuperUser/Kick/index.js
+++ packages/roleplay/commands/SuperUser/Kick/index.js
@@ -1,7 +1,8 @@
-mp.events.addCommandGroup('kick', ['superUser'], (player, id) => {
-    const newTarget = mp.players.at(id)
-    if(!newTarget) return player.outputChatBox("Syntax: /kick [playerID]");
-    if(newTarget === null) return player.outputChatBox("There is no player online with the ID given.")
-	newTarget.outputChatBox("You have been kicked from the server.");
-	newTarget.kick('Kicked.');
-})
+// mp.events.addCommandGroup('kick', ['superUser'], (player, id) => {
+//     const newTarget = mp.players.at(id)
+//     if(!newTarget) return player.outputChatBox("Syntax: /kick [playerID]");
+//     if(newTarget === null) return player.outputChatBox("There is no player online with the ID given.")
+//     console.log(`Player ${player.id} kicked ${id}`)
+// 	    newTarget.outputChatBox("You have been kicked from the server.");
+// 	    newTarget.kick('Kicked.');
+// })
