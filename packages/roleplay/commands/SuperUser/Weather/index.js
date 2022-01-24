43d02d5e80c3a1aac0535dbec98703231662dd31
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2022-01-24 02:31:25 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/LifeExpenrience/server-files""
    This reverts commit 52609ad5e3b5d2fd4b624c8c1fe4807d0745f619.

--- packages/roleplay/commands/SuperUser/Weather/index.js
+++ packages/roleplay/commands/SuperUser/Weather/index.js
@@ -1,34 +1,13 @@
-mp.events.addCommandGroup('settime', ['superUser'], (player, args) => {
-    args = args.split(' ');
-    const hour = args[0]
-    const minute = args[1]
-    console.log(`${hour}:${minute}`);
+const Weathers = ['EXTRASUNNY', 'CLEAR','CLOUDS','SMOG','FOGGY','OVERCAST','RAIN','THUNDER','CLEARING','NEUTRAL','SNOW','BLIZZARD','SNOWLIGHT','XMAS','HALLOWEEN']
+
+mp.events.addCommandGroup('setTime', ['superUser'], (player, _args, hour, minute) => {
     if(hour && minute) {
-        mp.world.time.set(parseInt(hour), parseInt(minute), 0);
-        player.notify(`You have set the time to ${hour}:${minute}`);
-    } else player.notify('Please enter a time.');    
-    args = null
-    player = null
+        mp.world.time.set(parseInt(hour), parseInt(minute), 0)
+        player.notify(`You have set the time to ${hour}:${minute}`)
+    } else player.notify('Please enter a time.')
 });
 
-mp.events.addCommandGroup('setweather', ['superUser'], (player, type) => {
-    const Weathers = [
-        'EXTRASUNNY',
-        'CLEAR',
-        'CLOUDS',
-        'SMOG',
-        'FOGGY',
-        'OVERCAST',
-        'RAIN',
-        'THUNDER',
-        'CLEARING',
-        'NEUTRAL',
-        'SNOW',
-        'BLIZZARD',
-        'SNOWLIGHT',
-        'XMAS',
-        'HALLOWEEN',
-    ];
+mp.events.addCommandGroup('setWeather', ['superUser'], (player, type) => {
     if(type) {
         if(Weathers.includes(type)) {
             mp.world.weather = type;
@@ -37,32 +16,11 @@ mp.events.addCommandGroup('setweather', ['superUser'], (player, type) => {
     }else player.notify('Please enter a weather type.');
 })
 
-mp.events.addCommandGroup('startweather', ['superUser'], (player, args) => {
-    args = args.split(' ');
-    const type = args[0]
-    const delay = args[1]
-    const Weathers = [
-        'EXTRASUNNY',
-        'CLEAR',
-        'CLOUDS',
-        'SMOG',
-        'FOGGY',
-        'OVERCAST',
-        'RAIN',
-        'THUNDER',
-        'CLEARING',
-        'NEUTRAL',
-        'SNOW',
-        'BLIZZARD',
-        'SNOWLIGHT',
-        'XMAS',
-        'HALLOWEEN',
-    ];
+mp.events.addCommandGroup('startWeather', ['superUser'], (player, _args, type, delay) => {
     if(type && delay) {
         if(Weathers.includes(type)) {
             mp.world.setWeatherTransition(type, parseInt(delay));
             player.notify(`You have set the weather to ${type}`);
         } else player.notify('Invalid weather type.');
     }else player.notify('Please enter a weather type and time.');
-    args = null
 })
