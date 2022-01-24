5c6a9cecc5d796d0d4ba3c4fd73d170277dbffb5
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2022-01-24 02:28:22 +0100

    Revert "Revert "fix(SuperUser): Optimised code""
    This reverts commit f9b08d4ba5520374717e0f6b4f81f79085abf3be.

--- packages/roleplay/commands/SuperUser/Vehicles/index.js
+++ packages/roleplay/commands/SuperUser/Vehicles/index.js
@@ -2,9 +2,9 @@ mp.events.addCommandGroup('veh', ['superUser'], (player, vehicle) => {
     player.spawnVehicle(vehicle, new mp.Vector3(player.position.x, player.position.y + 3.0, player.position.z), player.heading)
 });
 
-mp.events.addCommandGroup('setdim', ['superUser'], (player, _fullText, target, dimNumber) => {
+mp.events.addCommandGroup('setDim', ['superUser'], (player, _fullText, target, dimNumber) => {
     if (target == 'me') return player.dimension = Number(dimNumber);
-    if (!target || !dimNumber) return player.outputChatBox('Usage: /setdim [playerId | me], [dimension]');
+    if (!target || !dimNumber) return player.outputChatBox('Usage: /setDim [playerId | me], [dimension]');
     if (!mp.players.at(Number(target))) return player.outputChatBox('Player not found with ID ${target}');
     mp.players.at(Number(target)).dimension = Number(dimNumber);
 });
@@ -14,12 +14,11 @@ mp.events.addCommandGroup('fix', ['superUser'], player => {
 })
 
 mp.events.addCommandGroup('dv', ['superUser'], async (player, range) => {   
+    if( !range ) range = 1.5;
     if (player.vehicle) return player.deleteVehicle()
 
-    if( !range ) range = 1.5;
     mp.vehicles.forEachInRange(player.position, range, async vehicle => {
         if (vehicle.dimension != player.dimension) return
-
         await mp.utils.wait(1)
         vehicle.destroy();
     })
@@ -27,8 +26,8 @@ mp.events.addCommandGroup('dv', ['superUser'], async (player, range) => {
 
 
 mp.events.addCommandGroup('vehBring', ['superUser'], (player, vehId) => {
-    if (vehId == -1) return mp.vehicles.forEach(vehicle => vehicle.position = player.position)
     const vehicle = mp.vehicles.at(vehId);
+    if (vehId == -1) return mp.vehicles.forEach(vehicle => vehicle.position = player.position)
     if (vehicle) return vehicle.setPosition(player.position);
     player.outputChatBox("Vehicle not found.");
 });
