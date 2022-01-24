c675ca9a4ecf374fd83b5ffba7a4b6683f839545
Cristian <58856610+Apolo-sys@users.noreply.github.com>
2022-01-24 02:31:36 +0100

    Revert "fix(SuperUser): Optimised code"
    This reverts commit 6cb22b56a479cd3507c14910e984e202336cb10c.

--- packages/roleplay/commands/SuperUser/Health/index.js
+++ packages/roleplay/commands/SuperUser/Health/index.js
@@ -3,9 +3,7 @@ mp.events.addCommandGroup('heal', ['superUser'], (player, id) => {
     ( playerTarget ) ? player = playerTarget : player;
     
     player.health = 100;
-    player.shared.status = {
-        hunger: 100,
-        thirst: 100
-    }
+    player.shared.status.hunger = 100;
+    player.shared.status.thirst = 100;
     player.notify('You are healed')
 });
