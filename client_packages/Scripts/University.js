682ae4bb715ebb369f6b4dde1cfb7da24f42c6a2
NachoASD <nacheteasd@gmail.com>
2022-01-26 23:46:59 +0100

    Feat(University): Added working menu and finished totaly of it funcionality!

--- client_packages/Scripts/University.js
+++ client_packages/Scripts/University.js
@@ -3,19 +3,21 @@ let currentPoint = null
 
 let marker = null
 
+let menuOpened = false
+
 const drawPoint = async (point) => {
     if (marker) marker.destroy()
-    marker = await mp.markers.new(1, new mp.Vector3(point.x, point.y, point.z), 1.0, {
+    marker = await mp.markers.new(1, new mp.Vector3(point.menu.x, point.menu.y, point.menu.z), 1.0, {
         color: [255, 255, 255, 255],
         visible: true,
         dimension: 0
     })
 }
 
-
 mp.events.add("university:enter", ( point ) => {
     near++
     currentPoint = point
+    console.log('Near')
     drawPoint(point)
 });
 
@@ -33,6 +35,50 @@ mp.keys.bind(0x45, true, async() => {
     if (currentPoint == null) return
     if (mp.players.local.position.subtract(new mp.Vector3(currentPoint.menu.x, currentPoint.menu.y, currentPoint.menu.z)).length() >= 2.5) return
     
-    mp.events.callRemote('shop:university:action', [currentPoint]);
+    if(menuOpened) return
+
+    menuOpened = true
+    
+    const menuData = []
+
+    currentPoint.licenses.forEach(({name, label, description, price}, indx) => {
+        menuData[`${indx}`] = {
+            value:name,
+            label,
+            description,
+            dataOption:{
+                "Price": price
+            },
+            submitLabel: 'Buy license'
+        }
+    })
+
+    if(menuData.length == 0){
+        menuData.push({
+            value:"None",
+            label:"There are anything to see",
+            description: "There are anything to see, go out!",
+            submitLabel: 'Go out'
+        })
+    }
+
+    const menu = new mp.core.Menu(currentPoint.name, menuData)
+    
+    
+    menu.on('optionSelected', (option) => {
+        console.log(`optionSelected ${option}`)
+    })    
+    menu.on('optionClicked', async ({value}) => {
+        const response = await mp.events.callRemoteProc('university:action', value);
+        if(response.err){
+            mp.game.graphics.notify(response.err)            
+        }
+        menuOpened = false
+    })
+    menu.on('menuClosed', () => {
+        menuOpened = false
+    })
+
+    // mp.events.callRemote('shop:university:action', [currentPoint]);
 
 })
--- packages/roleplay/scripts/University/index.js
+++ packages/roleplay/scripts/University/index.js
@@ -3,27 +3,30 @@ const UniversityPoints = [
         name: "Chamberlain Hill University",
         blip: {
             name: "Chamberlain Hill University",
-            coords:{ x: -319.61,y: -1471.46,z: 30.12 },
-            sprite: 476,
+            //318.36 , -1631.69 , 32.53 , 217.97
+            coords:{ x: 318.36, y: -1631.69, z: 32.53 },
+            sprite: 475,
             color: 29            
         },
-        menu: new mp.Vector3(0,0,0),
+        menu: new mp.Vector3(318.36 , -1631.69 , 32.53),
         licenses: [
-            {name:'bus_license_1', label:'Bus License', description:'This is a bus license, with this bus license you can drive one of the multiple bus of the \'Sausage busline\'',price:3500}
+            {name:'bus_license_1', label:'Bus License lvl 1', description:'This is a bus license, with this bus license you can drive one of the multiple bus of the \'Sausage busline\'\n',price:3500},
+            {name:'bus_license_2', label:'Bus License lvl 2', description:'This is a bus license, with this bus license you can drive one of the multiple bus of the \'Sausage busline\'\n',price:5000},
+            {name:'bus_license_3', label:'Bus License lvl 3', description:'This is a bus license, with this bus license you can drive one of the multiple bus of the \'Sausage busline\'\n',price:7500}
         ]
     }
 ]
 
 const Init = () => {
     UniversityPoints.forEach(( point, index ) => {
-        const { menu, blip } = point
+        const { menu, blip, licenses, name } = point
         new mp.core.Blips(blip)
 
         const colshape = mp.colshapes.newRectangle(menu.x, menu.y, 3, 3, 0)
         
         function playerEnterColshapeHandler(player, shape) {
             if(shape == colshape) {
-                player.call("shop:university:enter", [{index,menu}])
+                player.call("university:enter", [{index,menu, licenses, name}])
             }
         }
         
@@ -32,7 +35,7 @@ const Init = () => {
         
         function playerExitColshapeHandler(player, shape) {
             if(shape == colshape) {
-                player.call("shop:university:exit", [{index,menu}])
+                player.call("university:exit", [{index,menu}])
             }
         }
         
@@ -40,42 +43,16 @@ const Init = () => {
     })
 }
 
-mp.events.add("shop:university:action", async (player, {index}) => {
-    //TODO (Menus) player.closeAllMenus()
-    //* let menuData = {}
-
-    //* UniversityPoints[index].licenses.forEach(({name, label, description, price}, indx) => {
-    //*     menuData[`${indx}`] = {
-    //*         value:name,
-    //*         label,
-    //*         description,
-    //*         dataOption:{
-    //*             price
-    //*         },
-    //*         submitLabel: 'Buy license'
-    //*     }
-    //* })
-
-    //* if(menuData.length == 0){
-    //*     menuData.push({
-    //*         value:"None",
-    //*         label:"There are anything to see",
-    //*         description: "There are anything to see, go out!",
-    //*         submitLabel: 'Go out'
-    //*     })
-    //* }
-
-    //* const menu = await player.openMenu(UniversityPoints[index].name, menuData)
-
-    //* menu.on('optionClicked', (option) => {
-    //*     if(option.price > player.balance) return {status:false, err:'You need more money to pay this'}
-    //*     if(player.shared.licenses == undefined){
-    //*         player.shared.licenses = {}
-    //*     }
-    //*     if(player.shared.licenses[`${option.name}`]) return {status:false, err:'You have buyed this license'}
-    //*     player.shared.licenses[`${option.name}`] = option
-    //*     return {status:true}
-    //* })
+mp.events.addProc("university:action", async (player, name) => {
+    const point = UniversityPoints.find(point => point.licenses.find(pto => pto.name == name) != undefined).licenses.find(pto => pto.name == name)
+    if(point.price > player.balance) return {status:false, err:'You need more money to pay this'}
+    if(player.shared.licenses == undefined){
+        player.shared.licenses = {}
+    }
+    if(player.shared.licenses[`${point.name}`]) return {status:false, err:'You have buyed this license'}
+    player.shared.licenses[`${point.name}`] = point
+    player.notify(`You bought ${point.label}`);
+    return {status:true}
 })
 
 Init()
