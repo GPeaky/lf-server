6ec188c8047e5b380baf7efec3909bb89eb7cdc3
GPeaky <gerardjoven2020@hotmail.com>
2022-01-26 18:25:08 +0100

    feat(database): deleted database loggins

--- packages/roleplay/config/database.js
+++ packages/roleplay/config/database.js
@@ -1,4 +1,3 @@
-const logger = require('logger').createLogger('./logs/database.log');
 const Sequelize = require('sequelize');
 
 const { SQ_NAME, SQ_USER, SQ_PASS } = process.env;
@@ -6,5 +5,5 @@ const { SQ_NAME, SQ_USER, SQ_PASS } = process.env;
 module.exports = new Sequelize(SQ_NAME, SQ_USER, SQ_PASS, {
     host: 'localhost',
     dialect: 'mariadb',
-    logging: log => logger.info(log)
+    logging: false
 })
