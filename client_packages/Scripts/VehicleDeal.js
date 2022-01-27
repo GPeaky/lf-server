404fab657c934ae9fd786196d5bad4fb58b79801
NachoASD <nacheteasd@gmail.com>
2022-01-28 00:19:17 +0100

    Feat(VehicleDeal): Started VehicleDeal with UI. Added preview
    TODO: Do the buy option

--- client_packages/Scripts/VehicleDeal.js
+++ client_packages/Scripts/VehicleDeal.js
@@ -3,9 +3,11 @@ let currentPoint = null
 
 let marker = null
 
+let menuOpened = false
+
 const drawPoint = async (point) => {
     if (marker) marker.destroy()
-    marker = await mp.markers.new(1, new mp.Vector3(point.x, point.y, point.z), 1.0, {
+    marker = await mp.markers.new(1, new mp.Vector3(point.menu.x, point.menu.y, point.menu.z), 1.0, {
         color: [255, 255, 255, 255],
         visible: true,
         dimension: 0
@@ -13,13 +15,13 @@ const drawPoint = async (point) => {
 }
 
 
-mp.events.add("VehicleDeal:enter", ( point ) => {
+mp.events.add("shop:VehicleDeal:enter", ( point ) => {
     near++
     currentPoint = point
     drawPoint(point)
 });
 
-mp.events.add("VehicleDeal:exit", ( ) => {
+mp.events.add("shop:VehicleDeal:exit", ( ) => {
     near--
     if (marker) {
         marker.destroy()
@@ -32,7 +34,60 @@ mp.keys.bind(0x45, true, async() => {
     if (near <=0) return
     if (currentPoint == null) return
     if (mp.players.local.position.subtract(new mp.Vector3(currentPoint.menu.x, currentPoint.menu.y, currentPoint.menu.z)).length() >= 2.5) return
+
+    if(menuOpened) return
+
+    menuOpened = true
     
-    mp.events.callRemote('shop:VehicleDeal:action', [currentPoint]);
+    const menuData = []
+
+    currentPoint.vehicles.forEach(({name, label, options, price}, indx) => {
+        options['Price'] = price
+        menuData[`${indx}`] = {
+            value:name,
+            label,
+            dataOption:options,
+            submitLabel: 'Buy Vehicle'
+        }
+    })
+
+    if(menuData.length == 0){
+        menuData.push({
+            value:"None",
+            label:"There are anything to see",
+            description: "There are anything to see, go out!",
+            submitLabel: 'Go out'
+        })
+    }
+    
+    const menu = new mp.core.Menu(currentPoint.name, menuData)
+    
+    let selected = null
+
+    menu.on('optionSelected', ({value}) => {
+        if(selected == value){
+            mp.events.callRemote("shop:VehicleDeal:stopPreview");
+            selected = null
+            return        
+        }else if(selected != null){
+            selected = value 
+            console.log(`optionSelected ${value}`)
+            mp.events.callRemote("shop:VehicleDeal:updatePreview", value);
+        }
+        selected = value 
+        console.log(`optionSelected ${value}`)
+        mp.events.callRemote("shop:VehicleDeal:startPreview", value);
+    })    
+    menu.on('optionClicked', async (option) => {
+        mp.events.callRemote("shop:VehicleDeal:stopPreview");
+        console.log(`optionClicked ${option}`)
+        menuOpened = false
+    })
+    menu.on('menuClosed', () => {
+        menuOpened = false
+    })
+
+    
+    // mp.events.callRemote('shop:VehicleDeal:action', [currentPoint]);
 
 })
--- packages/roleplay/scripts/VehicleDealer/index.js
+++ packages/roleplay/scripts/VehicleDealer/index.js
@@ -1,37 +1,46 @@
+let playersViwing = {}
+
 const VehicleDealPoints = [
     {
         name: "Chamberlain Hill Car Dealer",
         blip: {
             name: "Chamberlain Hill Car Dealer",
-            coords:{ x: -319.61,y: -1471.46,z: 30.12 },
+            // -44.1 , -1097.34 , 26.42 , 240.7
+            coords:{ x: -44.1, y: -1097.34, z: 26.42 },
             sprite: 810,
             color: 29            
         },
-        menu: new mp.Vector3(0,0,0),
+        menu: new mp.Vector3(-50.11, -1088.96, 26.42),
         vehiclePoint: {
-            coords: {
-                x:0,
-                y:0,
-                z:0
-            },
-            heading: 0
+            //-29.97 , -1087.12 , 25.81 , 340.21
+            coords: { x: -29.97, y: -1087.12, z: 25.81 },
+            heading: 340.21
+        },
+        vehiclePreview: {
+            //-42.97 , -1098.9 , 25.73 , 283.98
+            coords: { x: -42.97, y: -1098.9, z: 25.73 },
+            heading: 283.98
         },
         vehicles: [
-            {name:'t20', label:'Macaren P1', description:'Velocity: 120Km/h\nKilometers: 0Km', price:3500}
+            {name:'t20', label:'Macaren P1', options:{
+                'Velocity': '120Km/h',
+                'Kilometers': '0Km'
+            }, price:3500}
         ]
     }
 ]
 
 const Init = () => {
     VehicleDealPoints.forEach(( point, index ) => {
-        const { menu, blip } = point
+        const { menu, blip, vehicles } = point
         new mp.core.Blips(blip)
 
         const colshape = mp.colshapes.newRectangle(menu.x, menu.y, 3, 3, 0)
         
         function playerEnterColshapeHandler(player, shape) {
             if(shape == colshape) {
-                player.call("shop:VehicleDeal:enter", [{index,menu}])
+                console.log("A")
+                player.call("shop:VehicleDeal:enter", [{index,menu, vehicles}])
             }
         }
         
@@ -40,6 +49,7 @@ const Init = () => {
         
         function playerExitColshapeHandler(player, shape) {
             if(shape == colshape) {
+                console.log("B")
                 player.call("shop:VehicleDeal:exit", [{index,menu}])
             }
         }
@@ -48,20 +58,48 @@ const Init = () => {
     })
 }
 
-mp.events.add("shop:VehicleDeal:action", async (player, {index}) => {
-    // TODO (Menus) player.closeAllMenus()
-    //* const menuData = VehicleDealPoints[index].vehicles
-    // TODO Add the posibility of the colors
-    //* const menu = await player.openMenu(menuData)
-    
-    //* menu.onAccept((option) => {
-    //*     if(option.price > player.balance) return {status:false, err:'You need more money to pay this'}
-    
-            //TODO: Recibe the vehicle and set the color
-    //*     player.spawnVehicle(option.name, new mp.Vector3(option.vehiclePoint.coords.x, option.vehiclePoint.coords.y + 3.0, option.vehiclePoint.coords.z), option.vehiclePoint.heading)
+const PreviewVehicle = (model, {coords, heading}, player) => {
+    const veh = mp.vehicles.new(mp.joaat(model), new mp.Vector3(coords.x, coords.y, coords.z), {
+            heading: heading,
+            numberPlate: "LS482",
+            alpha: 255,
+            color: [[12, 12, 12], [12, 12, 12]],
+            locked: true,
+            engine: false,
+            dimension: player.id + 112
+        });
+    setTimeout( () => {
+        veh.exposition = true
+        try {
+            player.putIntoVehicle(veh, 0);            
+        } catch (error) {
+            return            
+        }
+    }, 200);
+    return veh
+}
 
-    //*     return {status:true}
-    //* })
+mp.events.add("shop:VehicleDeal:startPreview", (player, model) => {
+    console.log(model)
+    player.dimension = player.id + 112
+    playersViwing[player.id] = PreviewVehicle(model, VehicleDealPoints[0].vehiclePreview, player)
+})
+
+mp.events.add("shop:VehicleDeal:stopPreview", (player) => {
+    playersViwing[player.id].destroy()
+    playersViwing[player.id] = undefined
+    player.dimension = 0
+    player.position = new mp.Vector3(VehicleDealPoints[0].menu.x, VehicleDealPoints[0].menu.y, VehicleDealPoints[0].menu.z)    
+})
+
+mp.events.add("shop:VehicleDeal:updatePreview", (player, model) => {
+    player.dimension = player.id + 112
+    playersViwing[player.id].destroy()
+    playersViwing[player.id] = PreviewVehicle(model, VehicleDealPoints[0].vehiclePreview, player)
+})
+
+mp.events.add("shop:VehicleDeal:action", async (player, option) => {
+    const data = JSON.parse(option)
 })
 
 Init()
