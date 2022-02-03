be4e827493a9b3b7870c525f7d4fc2b352fa08bc
GPeaky <gerardjoven2020@hotmail.com>
2022-02-03 20:43:57 +0100

    feat(Server Commands): added revive

--- packages/roleplay/commands/SuperUser/Health/index.js
+++ packages/roleplay/commands/SuperUser/Health/index.js
@@ -7,3 +7,13 @@ mp.events.addCommandGroup('heal', ['superUser'], (player, id) => {
     player.shared.status.stamina = 100
     player.notify('You have been healed.')
 });
+
+mp.events.addCommandGroup('revive', ['superUser'], (player, id) => {
+    player = mp.players.at(id) ?? player
+    if (!player.isDead) return player.notify('You are not death')
+
+    player.spawn( player.position )
+    player.shared.isDead = false
+    player.shared.canRespawn = false
+    player.notify('You have been revived!')
+});
