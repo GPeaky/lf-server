65d2e9893dcc777052d4fc810f4dee3f9d90d8d2
Gerard Zinn <gerardjoven2020@hotmail.com>
2022-09-03 20:48:14 +0100

    Added pm2 and configuring conf.json

---
+++ pm2.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name   : "Life Experience",
+    script : "ragemp-server.exe",
+    watch: [
+      "ragemp-server.exe",
+      "./packages/*/**"
+    ],
+
+    restart: true
+  }]
+}
