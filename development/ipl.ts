775a7ae98b96a5480af79d70912fecc5b6e218d9
Gerard Zinn <gerardjoven2020@hotmail.com>
2022-09-04 02:23:23 +0100

    Fixed error with global mp interfaces

--- development/ipl.ts
+++ development/ipl.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 mp.world.requestIpl('vw_casino_carpark')
 mp.world.requestIpl('vw_casino_garage')
 mp.world.requestIpl('imp_impexp_interior_placement_interior_1_impexp_intwaremed_milo_')
